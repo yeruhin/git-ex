860e4f20a2abb95535a8753e3d2c8b94e6c80683
yeruhin <yeruhin@gmail.com>
2019-02-05 13:10:34 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict'
+function changeBcg(){
+    document.querySelector('body').style.backgroundColor = getRandomColor()
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
